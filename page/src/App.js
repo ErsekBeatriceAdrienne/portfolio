70d1cac712f655397cb8d045427fa8b7fe423661
Bea <99785877+ErsekBeatriceAdrienne@users.noreply.github.com>
2025-08-03 14:00:13 +0300

    homepage update

--- page/src/App.js
+++ page/src/App.js
@@ -1,21 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="app">
+      <header className="hero">
+        <h1>Welcome to my page!</h1>
+        <p>I'm a Computer Science graduate, and I'm ready for new challenges.</p>
+        <a href="https://github.com/ersekbeatriceadrienne" target="_blank" rel="noopener noreferrer">
+          Here's my GitHub profile →
         </a>
       </header>
     </div>
